83e9d5ee70783b8341d1094651531b5ee6060cce
Hunter0312 <nickyoungty.gmail.com>
2024-07-23 10:06:02 -0700

    make game speed level

---
+++ src/shared/gameParams.js
@@ -0,0 +1,16 @@
+const speedLevel1 = 4;
+const speedLevel2 = 7;
+const speedLevel3 = 10;
+
+const frameLevel1 = 120;
+const frameLevel2 = 90;
+const frameLevel3 = 60;
+
+export const levels = {
+    speedLevel1,
+    speedLevel2,
+    speedLevel3,
+    frameLevel1,
+    frameLevel2,
+    frameLevel3,
+}

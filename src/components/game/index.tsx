d3ec1dd0567d35ea5f2a8d6bef50ac68a8af2274
Hunter0312 <nickyoungty.gmail.com>
2024-08-06 04:39:14 -0700

    game screen cropping bug fix

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -259,7 +259,7 @@ const Game: React.FC<Props> = ({ setGameStatus, gameStatus, setScores, setRoulet
           }
           setScore(score => score + 1);
 
-          if (flagScore % 1 === 0 && flagScore >= 1) {
+          if (flagScore % 7 === 0 && flagScore >= 7) {
             if (Math.random() > 0.5) {
               setRoulette(true);
             }

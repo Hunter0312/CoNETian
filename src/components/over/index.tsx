e1ed4361d3540613900db8d90d7e7c77f5c91f52
nicolasstage <nicolas.maia@stage.consulting>
2024-08-09 15:27:31 -0300

    fix: fix gameover responsiveness

--- src/components/over/index.tsx
+++ src/components/over/index.tsx
@@ -23,22 +23,36 @@ const GameOver: React.FC<Props> = ({ setRestart, score, hScore }) => {
   const { setPath, profile } = useFlappyBirdContext();
 
   return (
-    <div className='flex flex-col justify-between items-center' style={{ height: "100%" }}>
-      <div className='flex flex-col justify-between'>
-        <p style={{ color: "white", fontSize: "5rem" }}>{score}</p>
+    <div className='flex flex-col' style={{
+      height: '100%',
+      justifyContent: 'space-evenly',
+      padding: '20px',
+      boxSizing: 'border-box',
+      overflowX: 'hidden',
+      overflowY: 'auto',
+      alignItems: 'center',
+      gap: '16px'
+    }}>
+      <div className='flex flex-col justify-between' style={{ gap: "4px" }}>
+        <div>
+          <p style={{ color: "white", fontSize: "2.5rem", margin: 0 }}>Current Score:</p>
+          <p style={{ color: "white", fontSize: "3rem", marginBlock: 0 }}>{score}</p>
+        </div>
+        <div>
+          <p style={{ color: "white", fontSize: "2.5rem", margin: 0, marginBlock: 0 }}>Highest Score:</p>
+          <p style={{ color: "white", fontSize: "3rem", margin: 0, marginBlock: 0 }}>{hScore}</p>
+        </div>
         {
-          profile && profile?.tokens?.CNTP?.balance !== 0 &&
+          profile && profile?.tokens?.CNTP?.balance >= 0 &&
           <>
             <p style={{ color: "white", fontSize: "2.5rem", margin: 0 }}>CNTP Balance:</p>
             <p style={{ color: "white", fontSize: "3rem", margin: 0, marginBottom: "2rem" }}>{profile?.tokens?.CNTP?.balance}</p>
           </>
         }
-        <p style={{ color: "white", fontSize: "2.5rem", margin: 0 }}>Highest Score:</p>
-        <p style={{ color: "white", fontSize: "3rem", margin: 0 }}>{hScore}</p>
       </div>
       <div className='flex flex-col align-center justify-between' style={{ gap: "16px" }}>
         <button onClick={setRestart} style={buttonStyle} className='startButton'>Restart</button>
-        <button onClick={() => setPath('/')} style={{ ...buttonStyle, marginBottom: "5rem" }}>
+        <button onClick={() => setPath('/')} style={buttonStyle}>
           Main Menu
         </button>
       </div>

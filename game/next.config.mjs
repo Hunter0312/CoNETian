e100b3c54298ddc4d5077baf6d5924b283be6fc5
nicolasstage <nicolas.maia@stage.consulting>
2024-09-13 09:53:48 -0300

    fix: fix next js build

--- game/next.config.mjs
+++ game/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { distDir: "out", reactStrictMode: false };
+const nextConfig = {
+  distDir: "out",
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
